ad5aa5ed99a03faea7c3fd1db1dc055490e17979
Peter von Tresckow <pvontres@amfam.com>
2020-11-16 19:21:12 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/PVT-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "gourav-network"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
