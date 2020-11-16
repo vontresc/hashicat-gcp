d34e5bbf7f3afae5f03c0d16f371220f58c2b8b0
Peter von Tresckow <pvontres@amfam.com>
2020-11-16 19:22:54 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -5,9 +5,10 @@ module "network" {
   network_name = "gourav-network"
   project_id = var.project
   subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+    ]
+}
